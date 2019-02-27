ac96a56902b1edd4e77b5adfd57a26d8efee0e66
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-02-26 22:35:24 -0500

    Update Password.js

--- Password.js
+++ Password.js
@@ -1,8 +1,12 @@
-generate = (length = 8) => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
-    const keys = alphabet + numbers + symbols;
+// Key pools
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789';
+const symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+
+// Function for randomly generating password
+generate = (length = 15) => {
+    const keys = lowercase + uppercase + numbers + symbols;
     let password = '';
     for (let i = 0; i < length; i++) {
         let index = Math.floor(Math.random() * keys.length);
@@ -11,4 +15,36 @@ generate = (length = 8) => {
     }
     return password;
 }
-console.log(generate())
+
+// Function for testing password strength (Using E = log_2(R^L), where 
+// E = entropy, R = pool size, L = password length)
+stren = (password) => {
+    // Calculate entropy
+    const keys = lowercase + uppercase + numbers + symbols;
+    let pool = keys.length;
+    let length = password.length;
+    let entropy = Math.log2(Math.pow(33, length));
+    // Obtain strength from entropy
+    let strength;
+    if (entropy < 28){
+        strength = 'Very Weak';
+    }
+    else if (entropy <= 35){
+        strength = 'Weak';
+    }
+    else if (entropy <= 59){
+        strength = 'Reasonable';
+    }
+    else if (entropy <= 127){
+        strength = 'Strong';
+    }
+    else {
+        strength = 'Very Strong';
+    }
+    return strength;
+}
+
+password = generate();
+strength = stren(password);
+console.log(`PASSWORD: ${password}`);
+console.log(`STRENGTH: ${strength}`);
