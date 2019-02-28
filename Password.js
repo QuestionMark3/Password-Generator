1425fe1a3df763a16e7a78d3e493b5cfb4125182
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2019-02-27 20:52:14 -0500

    Update Password.js

--- Password.js
+++ Password.js
@@ -4,9 +4,30 @@ const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789';
 const symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
 
+// Function for calculating intersection of two char arrays converted from strings
+intersection = (str1, str2) => {
+    let charArr1 = str1.split('');
+    let charArr2 = str2.split('');
+    let interArr = charArr1.filter( value => charArr2.includes(value) );
+    return interArr;
+}
+
 // Function for randomly generating password
-generate = (length = 15) => {
-    const keys = lowercase + uppercase + numbers + symbols;
+// Key Pool parameters are booleans that specifying what characters are used
+generate = (length = 12, sym = true, caps = true, num = true, low = true) => {
+    let keys = '';
+    if (low === true) {
+        keys += lowercase;
+    }
+    if (caps === true) {
+        keys += uppercase;
+    }
+    if (num === true) {
+        keys += numbers;
+    }
+    if (sym === true) {
+        keys += symbols;
+    }
     let password = '';
     for (let i = 0; i < length; i++) {
         let index = Math.floor(Math.random() * keys.length);
@@ -16,35 +37,50 @@ generate = (length = 15) => {
     return password;
 }
 
-// Function for testing password strength (Using E = log_2(R^L), where 
+// Function for testing password entropy (Using E = log_2(R^L), where 
 // E = entropy, R = pool size, L = password length)
-stren = (password) => {
+entropy = (password) => {
+    // Determine pool of characters
+    let keys = '';
+
+    for (let keyPool of [lowercase, uppercase, numbers, symbols]) {
+        let inter = intersection(password, keyPool);
+        if (inter.length !== 0) {
+            keys += keyPool;
+        }
+    }
+
     // Calculate entropy
-    const keys = lowercase + uppercase + numbers + symbols;
     let pool = keys.length;
     let length = password.length;
-    let entropy = Math.log2(Math.pow(33, length));
-    // Obtain strength from entropy
-    let strength;
-    if (entropy < 28){
-        strength = 'Very Weak';
-    }
-    else if (entropy <= 35){
-        strength = 'Weak';
-    }
-    else if (entropy <= 59){
-        strength = 'Reasonable';
-    }
-    else if (entropy <= 127){
-        strength = 'Strong';
-    }
-    else {
-        strength = 'Very Strong';
-    }
-    return strength;
+    let entrop = Math.log2(Math.pow(pool, length));
+    return entrop;
 }
 
-password = generate();
-strength = stren(password);
+// Obtain strength from entropy
+strength = (entrop) => {
+    let stren;
+    if (entrop < 28){
+        stren = 'Very Weak';
+    }
+    else if (entrop <= 35){
+        stren = 'Weak';
+    }
+    else if (entrop <= 59){
+        stren = 'Reasonable';
+    }
+    else if (entrop <= 127){
+        stren = 'Strong';
+    }
+    else {
+        stren = 'Very Strong';
+    }
+    return stren;
+}
+
+password = generate(20, false);
+entropy = entropy(password);
+strength = strength(entropy);
 console.log(`PASSWORD: ${password}`);
+console.log(` ENTROPY: ${entropy}`);
 console.log(`STRENGTH: ${strength}`);
